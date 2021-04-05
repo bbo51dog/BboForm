a127023ff5dee57e5b514cf30edb993d4edeffff
bbo51dog <bbo51dog@gmail.com>
2021-04-06 00:47:23 +0900

    Create base

---
+++ src/bbo51dog/bboform/element/Element.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace bbo51dog\bboform\element;
+
+use JsonSerializable;
+
+interface Element extends JsonSerializable {
+
+}
---
+++ src/bbo51dog/bboform/form/FormBase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace bbo51dog\bboform\form;
+
+use pocketmine\form\Form;
+
+abstract class FormBase implements Form {
+
+    public const TYPE_SIMPLE = "simple";
+    public const TYPE_MODAL = "modal";
+    public const TYPE_CUSTOM = "custom_form";
+
+    /** @var string */
+    private $title;
+
+    abstract public function getType(): string;
+
+    /**
+     * @return string|array
+     */
+    abstract public function serializeContent();
+
+    /**
+     * @inheritDoc
+     */
+    public function jsonSerialize(): array {
+        return [
+            "type" => $this->getType(),
+            "title" => $this->getTitle(),
+            "content" => $this->serializeContent(),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void {
+        $this->title = $title;
+    }
+}
